eed53e13ca228ea50c8cd207402db7cf00d31632
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-28 17:21:27 +0000

    Adds POST /albums and tests

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,4 +13,28 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  not_found do
+    status 404
+    return "Sorry! We couldn't find this post."
+  end
+
+  post '/albums' do
+    album = Album.new
+    album.title = params[:title]
+    album.release_year = params[:release_year]
+    album.artist_id = params[:artist_id]
+
+    album_repo = AlbumRepository.new
+    album_repo.create(album)
+    
+    return nil
+  end
+
+  get '/albums/:id' do
+    id = params[:id].to_i
+    album_repo = AlbumRepository.new
+    album = album_repo.find(id)
+    return "id=#{album.id},title=#{album.title},release_year=#{album.release_year},artist_id=#{album.artist_id}"
+  end
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,28 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "POST /albums" do
+    it 'returns 200 OK' do
+      post_response = post(
+        '/albums',
+        title: "Voyage",
+        release_year: 2022,
+        artist_id: 2
+      )
+
+      expect(post_response.status).to eq(200)
+
+      get_response = get('/albums/13')
+
+      expect(get_response.status).to eq 200
+      expect(get_response.body).to eq "id=13,title=Voyage,release_year=2022,artist_id=2"
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/albums?id=276278')
+
+      expect(response.status).to eq(404)
+      expect(response.body).to eq("Sorry! We couldn't find this post.")
+    end
+  end
 end
